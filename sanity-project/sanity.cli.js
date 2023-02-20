b21a78eda9bbedd883808e2638920b2d355445df
ajinkya <kotwalajinkya@yahoo.com>
2023-02-20 15:18:30 +0530

    https://github.com/ajinkyakotwal/portfolio

---
+++ sanity-project/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '8p89dqu9',
+    dataset: 'production'
+  }
+})
---
+++ schemas/blog.js
@@ -0,0 +1,25 @@
+export default {
+    name: 'blog',
+    type: 'document',
+      title: 'Blog',
+    fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Title'
+      },
+      
+      {
+        name: 'content',
+        type: 'string',
+        title: 'Content'
+      },
+
+      {
+        title: 'Launch Scheduled At',
+        name: 'launchAt',
+        type: 'datetime'
+      },
+
+    ]
+  }
